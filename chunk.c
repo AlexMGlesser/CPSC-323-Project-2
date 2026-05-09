0467af7fdd31ade0d7d369d401aee7155db79e7e
Alex Glesser <alexglesser@csu.fullerton.edu>
2026-05-09 16:38:12 -0700

    Finished chapter 14

--- chunk.c
+++ chunk.c
@@ -24,6 +24,8 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code,
         oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines,
+        oldCapacity, chunk->capacity);
   }
 int addConstant(Chunk* chunk, Value value) {
   writeValueArray(&chunk->constants, value);
